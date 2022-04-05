5acc9a1554dd461b26b95441cac183a8cbe3a832
budhamud <nigitart@gmail.com>
2022-04-04 21:29:58 -0300

    new branch

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".h").classList.toggle("show");
+});
+
+// function checkAge(age) {
+//     if (age >= 18) {
+//         return true;
+//     } else {
+//         return confirm("¿Tus viejos saben de esto?")
+//     }
+// }
+
+// let age = prompt('Qué edad tenés?');
+
+// if ( checkAge(age) ) {
+//     alert('Adelante maestro');
+//     } else {
+//     alert("No te puedo echar, pero si te vas me hacés un favor");
+// }
+
+// alert(check(age));
+
